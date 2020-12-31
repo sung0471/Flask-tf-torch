389ffcf50c0f1efb0cc9173598ab8ed1c9709400
PSM <zzererer@naver.com>
2021-01-01 06:12:10 +0900

    210101 commit - 2 webcam.py
    - webcam을 사용하는 케이스 추가
    - 동영상에서 frame 추출이 끝나면 종료시키는 예외 추가
    - location 정보를 url의 get방식으로 추가
    - 그 밖에 오타 수정

--- webcam.py
+++ webcam.py
@@ -1,30 +1,37 @@
 import requests
-import json
 import cv2
 import time
 from utils.utils import FileControl
 
 path = FileControl()
 
-url = "http://localhost:5000/tracking"
-cnt = 0
-mp4_name = path.get_video_path('box.mp4')
-video_capture = cv2.VideoCapture(mp4_name)
-payload = {}
+ip = 'localhost'
+url = f"http://{ip}:5000/tracking"
+use_webCam = True
+if use_webCam:
+    file_name = 0
+    location = 1
+else:
+    file_name = path.get_video_path('box.mp4')
+    location = 0
+video_capture = cv2.VideoCapture(file_name)
 
+payload = {}
 headers = {}
 idx = 0
 start_point = 0
 while True:
     ret, frame = video_capture.read()
+    if not ret:
+        break
     if idx >= start_point:
         _, img_encoded = cv2.imencode('.jpg', frame)
 
         files = [
-            ('file', (f'fram{idx}.jpg', img_encoded.tostring(), 'image/jpeg'))
+            ('file', (f'frame{idx}.jpg', img_encoded.tostring(), 'image/jpeg'))
         ]
         # URL 번호 frame에서 time으로 교체 해야함!
-        url_full = url + f'?frame={idx}'
+        url_full = url + f'?location={location}&frame={idx}'
 
         check = True
         while check:
@@ -36,8 +43,7 @@ while True:
                 print(response.text)
                 print('request time : ', time.time() - start)
                 check = False
+                idx += 1
             except Exception as e:
                 print(e)
                 cv2.waitKey(1)
-
-    idx += 1
